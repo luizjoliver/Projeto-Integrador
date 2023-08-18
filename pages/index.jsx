46b62e422b5d9b0fa1f02be49437ada91a191ac0
Luiz fellipe Joliver Gonçalves e silva <106269210+luizjoliver@users.noreply.github.com>
2023-08-18 14:08:45 -0300

    Finalizando a criação do cardSection re utilizável e arrumando estilizações via props do cardSection

--- pages/index.jsx
+++ pages/index.jsx
@@ -17,7 +17,8 @@ export default function Home() {
     </Head>
     <HeaderNoAuth/>
       <PresentationCardSection/>
-      <CardSection/>
+      <CardSection imgUrl="https://petanjo.com/blog/wp-content/uploads/2021/06/meu-gato-nao-para-de-miar-709x384.jpg" directionRigth={false}/>
+      <CardSection directionRigth={true} imgUrl="https://veja.abril.com.br/wp-content/uploads/2017/01/cao-labrador-3-copy.jpg?quality=90&strip=info&w=1280&h=720&crop=1"/>
     <Footer/>
     </>
   )
--- src/components/homeNoAuth/cardSection/index.jsx
+++ src/components/homeNoAuth/cardSection/index.jsx
@@ -1,14 +1,14 @@
 import styles from "./styles.module.scss"
 
-const CardSection = () =>{
+const CardSection = ({directionRigth,imgUrl}) =>{
     return(
         <>
-            <section className={styles.card1}>
-                 <div className={styles.imgCard1}>
+            <section className={directionRigth ? styles.card1 : styles.card2}>
+                 <div className={styles.imgCard1} style={{backgroundImage: `url(${imgUrl}})`}}>
 
                  </div>
                  <div className={styles.cardContent}>
-                    <h2 style={styles.title}>Lorem ipsum</h2>
+                    <h2 className={styles.title}>Lorem ipsum</h2>
                     <p className={styles.description}>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iure et, dignissimos rem, esse odio non nisi labore quidem excepturi maiores explicabo placeat, nulla magni alias ut autem ad? Soluta, fugiat?</p>
                  </div>
             </section>
